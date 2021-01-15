9a38b153e9a729b1c703052c34484d810dc45d4a
junghyomoon <dsinhome70@gmail.com>
2021-01-15 20:00:49 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector(".clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+function init() {
+
+}
+
+setInterval(getTime, 1000);
+init();
